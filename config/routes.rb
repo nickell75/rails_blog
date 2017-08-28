7421318207e854c8af11b25789e164631dba7a71
jnickell <nickell75@yahoo.com>
2017-08-28 08:05:45 -0700

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
 
   mount Ckeditor::Engine => '/ckeditor'
 	
+  get 'posts/index'
 	root 'posts#index'
-	resources :posts do 
-		resources :comments
-	end
+	resources :posts 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
