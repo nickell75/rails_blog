76ca748b6a1c72fe2d0cd181a573f5c9b3c6d19f
jnickell <nickell75@yahoo.com>
2017-08-27 17:23:21 -0700

    Adds comments and validations for posts

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 	# delete 	'/posts/:id' => 'posts#destroy'
 
 	root to: 'posts#index'
-	resources :posts
+	resources :posts do 
+		resources :comments
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
