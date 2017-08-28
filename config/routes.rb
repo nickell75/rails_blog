050a57d7c158008bd3e7a756e6b241a4b76e92c9
jnickell <nickell75@yahoo.com>
2017-08-27 17:10:23 -0700

    Adds posts controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  mount Ckeditor::Engine => '/ckeditor'
+	# get 		'/posts' => 'posts#index'
+	# get 		'/posts/:id' => 'posts#show'
+	# get 		'/posts/new' => 'posts#new'
+	# get 		'/posts/:id/edit' => 'posts#edit'
+	# post 		'/posts' => 'posts#create'
+	# patch 	'/posts/:id' => 'posts#update'
+	# delete 	'/posts/:id' => 'posts#destroy'
+
+	root to: 'posts#index'
+	resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
