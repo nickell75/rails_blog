b3b460cdeb9083f461c444c0b77c324568dfe033
jnickell <nickell75@yahoo.com>
2017-08-27 18:02:49 -0700

    Added Gemfile.lock

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
 
   mount Ckeditor::Engine => '/ckeditor'
-	# get 		'/posts' => 'posts#index'
-	# get 		'/posts/:id' => 'posts#show'
-	# get 		'/posts/new' => 'posts#new'
-	# get 		'/posts/:id/edit' => 'posts#edit'
-	# post 		'/posts' => 'posts#create'
-	# patch 	'/posts/:id' => 'posts#update'
-	# delete 	'/posts/:id' => 'posts#destroy'
-
+	
 	root to: 'posts#index'
 	resources :posts do 
 		resources :comments
